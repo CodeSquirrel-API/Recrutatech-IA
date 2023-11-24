85824d1ed74b614c301c6d7910214fde74e773b4
Frederico Rabelo <frederico.rabelo@triasoftware.com>
2023-11-24 11:39:11 -0300

    Chamando api

--- backend.py
+++ backend.py
@@ -3,28 +3,20 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 def minha_funcao(payload):
+    profissao = payload.get('profissao', '')
+    conhecimento = payload.get('conhecimento', [])
+    habilidade = payload.get('habilidade', [])
+    atitude = payload.get('atitude', [])
 
-    name = payload.get('name', '')
-    knowledge = payload.get('knowledge', '')
-    skill = payload.get('skill', '')
-    attitude = payload.get('attitude', '')
-    experience = payload.get('experience', '')
-
-
-    resultado = f"{name}! Conhecimento: {knowledge}, Habilidade: {skill}, Atitude: {attitude}, Experiência: {experience}"
+    resultado = f"Profissão: {profissao}, Conhecimento: {conhecimento}, Habilidade: {habilidade}, Atitude: {atitude}"
 
     return resultado
 
 @app.route('/colaborador', methods=['POST'])
 def rota_principal():
-
     if request.method == 'POST' and request.is_json:
-
         payload = request.get_json()
-
-        
         resultado = minha_funcao(payload)
-
         return jsonify({"resultado": resultado})
     else:
         return jsonify({"error": "Solicitação inválida"}), 400
